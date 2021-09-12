a221514518ff6c14049a4e7633810758e216dd8f
Okpo Kingsley Francis <kingsleyfrancis42@gmail.com>
2021-09-12 09:45:23 +0100

    react projectcommit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import Page from './Components/nav';
+//import box from './Components/box/box';
+
+
+
+
+function App(){
+    return(
+        <div>
+         <h1> People</h1>
+        <Page></Page>
+        </div>
+    )
+}
+export default App;
